b9e97461cf51f73c2620fc43940e78ef131bb06c
Swifter <swifter1243@gmail.com>
2022-07-23 00:09:46 -0400

    setup

---
+++ script.py
@@ -0,0 +1,8 @@
+bl_info = {
+    "name": "ReMapper Exporter",
+    "author": "Swifter",
+    "version": "0.01",
+    "blender": (2, 80, 0),
+    "location": "View3d > Sidepanel",
+    "description": "Blender Plugin to export scenes into models which are used by ReMapper."
+}
