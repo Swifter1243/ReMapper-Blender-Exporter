3bde1478c196cfe189c63b904c1d21e347a9673f
Swifter <swifter1243@gmail.com>
2022-12-18 23:10:32 -0500

    new version

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ from mathutils import Euler, Matrix
 bl_info = {
     "name": "ReMapper Exporter",
     "author": "Swifter",
-    "version": "0.03",
+    "version": "0.04",
     "blender": (2, 80, 0),
     "location": "View3d > Sidepanel",
     "description": "Blender Plugin to export scenes into models which are used by ReMapper."
